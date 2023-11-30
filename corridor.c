8262ec35f5629b2baa56bf24cad55ef0a08f853a
TenixOue <79940202+TenixOue@users.noreply.github.com>
2023-11-30 13:56:31 +0100

    depassement du couloit par donjon et room

--- corridor.c
+++ corridor.c
@@ -6,9 +6,11 @@
 dungeon corridorPart(dungeon monDonjon, int i, int j, int number) {
     switch (number) {
         case 1:
-            monDonjon.chunks[i - 1][j] = '#';
-            monDonjon.chunks[i][j] = ' ';
-            monDonjon.chunks[i + 1][j] = '#';
+            if(monDonjon.chunks[i][j] != '#' || monDonjon.chunks[i-1][j] != '#' || monDonjon.chunks[i+1][j] != '#'){
+                monDonjon.chunks[i - 1][j] = '#';
+                monDonjon.chunks[i][j] = ' ';
+                monDonjon.chunks[i + 1][j] = '#';
+            }
             break;
         case 2:
             monDonjon.chunks[i][j-1] = '#';
@@ -25,94 +27,90 @@ dungeon corridorPart(dungeon monDonjon, int i, int j, int number) {
     }return monDonjon;
 }
 
-// int collisionCorridorRoom(dungeon monDonjon, int i, int j){
-//     if()
-// }
+int collisionCorridorRoom(dungeon monDonjon, int i, int j){
+    
+}
 
-void makeCorridor(dungeon monDonjon, int i, int j){
+void makeCorridor(dungeon monDonjon, int i, int j) {
     int choice = 10;
     int preChoice = 0;
-    while(choice!=0){
-        printf("Quelle direction ? (1 =>  / 2 =>  / 3 =>  / 4 => ) ");
-        scanf("\n %d",&choice);
-        if(i - 1 >= 0 && j+1 < monDonjon.largeur && j - 1 >= 0 && i+1 < monDonjon.longueur){
-            switch(choice){
+    while (choice != 0) {
+        scanf("\n %d", &choice);
+        if(monDonjon.chunks[i][j] == '#' || monDonjon.chunks[i-1][j] == '#' || monDonjon.chunks[i+1][j] == '#'){
+            choice = 0; 
+        }
+        if (i - 1 >= 0 && j + 1 < monDonjon.largeur && j - 1 >= 0 && i + 1 < monDonjon.longueur) {
+            switch (choice) {
                 case 1:
+                    
                     corridorPart(monDonjon, i, j, 1);
-                    if(preChoice == 2 && choice == 1){
-                        corridorPart(monDonjon, i, j-1, 1);
-                        monDonjon.chunks[i][j-1] = '#';
-                        monDonjon.chunks[i-1][j] = ' ';
-                    } else if(preChoice == 3 && choice == 1){
-                        corridorPart(monDonjon, i, j-1, 1);
-                        monDonjon.chunks[i][j-1] = '#';
-                        monDonjon.chunks[i+1][j] = ' ';
+                    if (preChoice == 2 && choice == 1) {
+                        corridorPart(monDonjon, i, j - 1, 1);
+                        monDonjon.chunks[i][j - 1] = '#';
+                        monDonjon.chunks[i - 1][j] = ' ';
+                    } else if (preChoice == 3 && choice == 1) {
+                        corridorPart(monDonjon, i, j - 1, 1);
+                        monDonjon.chunks[i][j - 1] = '#';
+                        monDonjon.chunks[i + 1][j] = ' ';
                     }
                     j++;
                     preChoice = choice;
-
-                
                     printDungeon(monDonjon);
-
-
                     break;
                 case 2:
-                    // if (i - 1 >= 0 && j+1 < monDonjon.largeur && j - 1 >= 0) {
-                        corridorPart(monDonjon, i, j, 2);
-                        if(preChoice == 1 && choice == 2){
-                            monDonjon.chunks[i-1][j] = '#';
-                            monDonjon.chunks[i-1][j+1] = '#';
-                            monDonjon.chunks[i][j-1] = ' ';
-                        } else if(preChoice == 4 && choice == 2){
-                            corridorPart(monDonjon, i-1, j, 2);
-                            monDonjon.chunks[i-1][j] = '#';
-                            monDonjon.chunks[i][j+1] = ' ';
-                        }
-                        i++;
-                        preChoice = choice;
-
-                    // }
+                // if (i - 1 >= 0 && j + 1 < monDonjon.largeur && j - 1 >= 0) {
+                    corridorPart(monDonjon, i, j, 2);
+                    if (preChoice == 1 && choice == 2) {
+                        monDonjon.chunks[i - 1][j] = '#';
+                        monDonjon.chunks[i - 1][j + 1] = '#';
+                        monDonjon.chunks[i][j - 1] = ' ';
+                    } else if (preChoice == 4 && choice == 2) {
+                        corridorPart(monDonjon, i - 1, j, 2);
+                        monDonjon.chunks[i - 1][j] = '#';
+                        monDonjon.chunks[i][j + 1] = ' ';
+                    }
+                    i++;
+                    preChoice = choice;
                     printDungeon(monDonjon);
+                // }
                     break;
                 case 3:
-                    // if (i - 1 >= 0 && j+1 < monDonjon.largeur && j - 1 >= 0) {
-                        corridorPart(monDonjon, i, j, 2);
-                        if(preChoice == 1 && choice == 3){
-                            corridorPart(monDonjon, i+1, j, 2);
-                            monDonjon.chunks[i+1][j] = '#';
-                            monDonjon.chunks[i][j-1] = ' ';
-                        } else if(preChoice == 4 && choice == 3){
-                            corridorPart(monDonjon, i+1, j, 2);
-                            monDonjon.chunks[i+1][j] = '#';
-                            monDonjon.chunks[i][j+1] = ' ';
-                        }
-                        i--;
-                        preChoice = choice;
-
-                    // }
+                // if (i - 1 >= 0 && j + 1 < monDonjon.largeur && j - 1 >= 0) {
+                    corridorPart(monDonjon, i, j, 2);
+                    if (preChoice == 1 && choice == 3) {
+                        corridorPart(monDonjon, i + 1, j, 2);
+                        monDonjon.chunks[i + 1][j] = '#';
+                        monDonjon.chunks[i][j - 1] = ' ';
+                    } else if (preChoice == 4 && choice == 3) {
+                        corridorPart(monDonjon, i + 1, j, 2);
+                        monDonjon.chunks[i + 1][j] = '#';
+                        monDonjon.chunks[i][j + 1] = ' ';
+                    }
+                    i--;
+                    preChoice = choice;
                     printDungeon(monDonjon);
+                // }
                     break;
                 case 4:
-                    // if (j - 1 >= 0) {
-                        corridorPart(monDonjon, i, j, 1);
-                        if(preChoice == 1 && choice == 4){
-                            monDonjon.chunks[i-1][j] = '#';
-                            monDonjon.chunks[i-1][j+1] = '#';
-                            monDonjon.chunks[i][j-1] = ' ';
-                        } else if(preChoice == 2 && choice == 4){
-                            corridorPart(monDonjon, i, j+1, 1);
-                            monDonjon.chunks[i][j+1] = '#';
-                            monDonjon.chunks[i-1][j] = ' ';
-                        } else if(preChoice == 3 && choice == 4){
-                            corridorPart(monDonjon, i, j+1, 1);
-                            monDonjon.chunks[i][j+1] = '#';
-                            monDonjon.chunks[i+1][j] = ' ';
-                        // }
-                        j--;
-                        preChoice = choice;
-
+                // if (j - 1 >= 0) {
+                    corridorPart(monDonjon, i, j, 1);
+                    if (preChoice == 1 && choice == 4) {
+                        monDonjon.chunks[i - 1][j] = '#';
+                        monDonjon.chunks[i - 1][j + 1] = '#';
+                        monDonjon.chunks[i][j - 1] = ' ';
+                    } else if (preChoice == 2 && choice == 4) {
+                        corridorPart(monDonjon, i, j + 1, 1);
+                        monDonjon.chunks[i][j + 1] = '#';
+                        monDonjon.chunks[i - 1][j] = ' ';
+                    } else if (preChoice == 3 && choice == 4) {
+                        corridorPart(monDonjon, i, j + 1, 1);
+                        monDonjon.chunks[i][j + 1] = '#';
+                        monDonjon.chunks[i + 1][j] = ' ';
                     }
+                    j--;
+                    preChoice = choice;
                     printDungeon(monDonjon);
+                // }
                     break;
                 default:
                     break;
